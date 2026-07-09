f4aa51649b2c1499607bc03fd3af964abd3ee552
xavierr-max <maxwelldearaujo1017@gmail.com>
2026-07-09 14:14:03 -0300

    feat(index): Novos recursos na rota de HealthCheck

--- src/index.js
+++ src/index.js
@@ -10,7 +10,14 @@ app.use(router);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('API de cadastro de animais');
+    const healthCheck = {
+        status: 'online',
+        mensagem: 'API de cadastro de animais operante',
+        timestamp: new Date().toISOString(),
+        uptime: `${Math.floor(process.uptime())} segundos`
+    };
+
+    res.status(200).json(healthCheck);
 });
 
 app.listen(port, () => {
